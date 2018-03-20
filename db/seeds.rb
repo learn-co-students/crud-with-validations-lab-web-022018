9ef97edd52e1539d32013cf01f699795ac2effcf
alec-petros <a.petros@verizon.net>
2018-03-20 15:31:21 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Song.create(title: "Crazy", artist_name: "Lido", released: true, genre: "Future Bass", release_year: 2016)
+Song.create(title: "Core", artist_name: "RL Grime", released: true, genre: "Trap", release_year: 2014)
+Song.create(title: "Scary Monsters", artist_name: "Skrillex", released: true, genre: "Dubstep", release_year: 2010)
+Song.create(title: "Transform", artist_name: "G Jones", released: true, genre: "West Coast Bass", release_year: 2017)
+Song.create(title: "Humanoid 2.0", artist_name: "EPROM", released: false, genre: "Slimepunk")
